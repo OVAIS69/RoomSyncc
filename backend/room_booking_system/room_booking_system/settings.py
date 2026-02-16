f61487865eb5eafc77930f724c2167a3eaf71955
Ovais <ovais.0404@gmail.com>
2026-02-16 13:12:06 +0530

    Fix: Add STATIC_ROOT and Whitenoise for production static files

--- backend/room_booking_system/room_booking_system/settings.py
+++ backend/room_booking_system/room_booking_system/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -147,6 +148,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',
+# )
+
+# Enable WhiteNoise's GZip compression of static assets.
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
